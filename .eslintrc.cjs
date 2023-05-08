dec5ec67b13a73333299c45f608b9a9025c2cf5e
codingxxxxx <hc2.minea@gamil.com>
2023-05-08 09:37:02 +0700

    chore: configure eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,6 +3,11 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
@@ -10,5 +15,8 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    quotes: ['error', 'single']
   }
 }
